050aba7b1cf3e9eee1fc59da424baeb6a5126a66
Anika Tabassum <anikatabassum2738@gmail.com>
2026-09-18 00:04:58 +0600

    exercises - max of two numbers, landscape or potrait, fizzbuzz, demerit points

---
+++ 28-exercise-max-of-two-numbers/index.js
@@ -0,0 +1,13 @@
+// write afunction that takes two numbers and returns the maximum of the two.
+
+let number = max(1, 3);
+console.log(number);
+
+function max(a, b) {
+    if (a > b) return a;
+    else return b;
+
+    // return (a > b) ? a : b;
+}
+
+// (condition) ? a : b
---
+++ 30-exercise-fizzbuzz/index.js
@@ -0,0 +1,25 @@
+// divisible by 3 => Fizz
+// divisible by 5 => Buzz
+// divisible by 3 and 5 => FizzBuzz
+// not divisible by 3 or 5 => input
+// not a number => 'Not a number'
+
+const output = fizzBuzz(15);
+console.log(output);
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+        return 'Not a number';
+    
+    if ((input % 3 === 0) && (input % 5 === 0))
+        return 'FizzBuzz';
+    
+    if (input % 3 === 0)
+        return 'Fizz';
+
+    if (input % 5 === 0)
+        return 'Buzz';
+
+
+    return input;
+}
